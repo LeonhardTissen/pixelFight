9e1d60b81d13be78d7a0b73ad951611d71d23464
WarzeEgg <contact@warze.org>
2022-08-03 12:58:55 +0200

    Great render optimization

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const countb = document.getElementById('countb');
 const hash = parseInt(location.hash.replace('#',''))
 const s = (hash > 0 ? hash : 64)
 cvs.width = cvs.height = s;
+let speed = 20;
 const ctx = cvs.getContext('2d');
 
 const pixels = Array(s).fill().map(() => Array(s).fill(0));
@@ -25,9 +26,21 @@ const a = [
 
 let t = 0;
 let over = false;
+
+// render
+ctx.fillStyle = 'red';
+let red_pixels = 0;
+for (let y = 0; y < s; y ++) {
+    for (let x = 0; x < s; x ++) {
+        if (pixels[x][y] === 1) {
+            red_pixels ++;
+            ctx.fillRect(x, y, 1, 1);
+        };
+    };   
+};
+
 function tick() {
     if (over) {
-        alert("End")
         return;
     }
     t ++;
@@ -35,7 +48,7 @@ function tick() {
     // fight
     let things_changed = 0;
     let nothing_happened = 0;
-    while (things_changed < 5 && nothing_happened < 10000) {
+    while (things_changed < speed && nothing_happened < 10000) {
         const rx = Math.floor(Math.random() * (s - 2)) + 1;
         const ry = Math.floor(Math.random() * (s - 2)) + 1;
     
@@ -48,10 +61,26 @@ function tick() {
         }
         if (Math.random() > surv_chance) {
             pixels[rx][ry] = (victim === 1 ? 0 : 1);
+            if (victim === 1) {
+                ctx.clearRect(rx, ry, 1, 1);
+                red_pixels --;
+            } else {
+                ctx.fillRect(rx, ry, 1, 1);
+                red_pixels ++;
+            }
         } else {
             const d = a[Math.floor(Math.random() * 4)];
-            if (pixels[rx + d[0]][ry + d[1]] !== victim) {
-                pixels[rx + d[0]][ry + d[1]] = victim;
+            const ex = rx + d[0];
+            const ey = ry + d[1]
+            if (pixels[ex][ey] !== victim) {
+                pixels[ex][ey] = victim;
+                if (victim === 0) {
+                    ctx.clearRect(ex, ey, 1, 1);
+                    red_pixels --;
+                } else {
+                    ctx.fillRect(ex, ey, 1, 1);
+                    red_pixels ++;
+                }
                 things_changed ++;
                 nothing_happened = 0;
             } else {
@@ -62,19 +91,6 @@ function tick() {
     if (nothing_happened == 10000) {
         over = true;
     }
-
-    // render
-    ctx.clearRect(0, 0, s, s);
-    ctx.fillStyle = 'red';
-    let red_pixels = 0;
-    for (let y = 0; y < s; y ++) {
-        for (let x = 0; x < s; x ++) {
-            if (pixels[x][y] === 1) {
-                red_pixels ++;
-                ctx.fillRect(x, y, 1, 1);
-            };
-        };   
-    };
 
     const percentage = Math.round(red_pixels / (s * s) * 100);
     counta.innerText = percentage + '%';
