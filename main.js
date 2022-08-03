55e53c7dc6496f42947ff19ed9c0275522994adb
WarzeEgg <contact@warze.org>
2022-08-03 12:48:14 +0200

    Hash, better layout

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const cvs = document.getElementById('screen');
-const count = document.getElementById('count');
-const s = 64
+const counta = document.getElementById('counta');
+const countb = document.getElementById('countb');
+const hash = parseInt(location.hash.replace('#',''))
+const s = (hash > 0 ? hash : 64)
 cvs.width = cvs.height = s;
 const ctx = cvs.getContext('2d');
 
@@ -8,7 +10,7 @@ const pixels = Array(s).fill().map(() => Array(s).fill(0));
 
 for (let y = 0; y < s; y ++) {
     for (let x = 0; x < s; x ++) {
-        if (x < s / 2) {
+        if (y >= s / 2) {
             pixels[x][y] = 1;
         }
     };   
@@ -22,12 +24,18 @@ const a = [
 ]
 
 let t = 0;
+let over = false;
 function tick() {
+    if (over) {
+        alert("End")
+        return;
+    }
     t ++;
 
     // fight
     let things_changed = 0;
-    while (things_changed < 5) {
+    let nothing_happened = 0;
+    while (things_changed < 5 && nothing_happened < 10000) {
         const rx = Math.floor(Math.random() * (s - 2)) + 1;
         const ry = Math.floor(Math.random() * (s - 2)) + 1;
     
@@ -45,8 +53,14 @@ function tick() {
             if (pixels[rx + d[0]][ry + d[1]] !== victim) {
                 pixels[rx + d[0]][ry + d[1]] = victim;
                 things_changed ++;
+                nothing_happened = 0;
+            } else {
+                nothing_happened ++;
             }
         }
+    }
+    if (nothing_happened == 10000) {
+        over = true;
     }
 
     // render
@@ -63,14 +77,8 @@ function tick() {
     };
 
     const percentage = Math.round(red_pixels / (s * s) * 100);
-    if (percentage < 50) {
-        count.style.color = 'blue';
-    } else if (percentage > 50) {
-        count.style.color = 'red';
-    } else {
-        count.style.color = 'white';
-    }
-    count.innerText = percentage + "%"
+    counta.innerText = percentage + '%';
+    countb.innerText = 100 - percentage + '%';
 
     window.requestAnimationFrame(tick);
 }
